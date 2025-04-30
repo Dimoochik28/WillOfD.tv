2aed51c3598d3f41772574bbaf165b91a794dd9f
Dimoochik28 <148957099+Dimoochik28@users.noreply.github.com>
2025-04-30 10:27:28 +0300

    Add files via upload

---
+++ data/episods.js
@@ -0,0 +1,56 @@
+window.animeData = {
+  onepiece: {
+    title: "One Piece",
+    description: "Пригоди піратів у пошуках One Piece.",
+    seasons: [
+      {
+        seasonTitle: "",
+        episodes: 1127,
+      },
+    ],
+  },
+  naruto: {
+    title: "Naruto",
+    description: "Історія про ніндзя",
+    seasons: [
+      {
+        seasonTitle: "Сезон 1",
+        episodes: 220,
+      },
+      {
+        seasonTitle: "Сезон 2",
+        episodes: 500,
+      },
+    ],
+  },
+  boruto: {
+    title: "Boruto",
+    episodes: 293,
+    description: "Продовження історії про ніндзя",
+  },
+  bleach: {
+    title: "Bleach",
+    episodes: 366,
+    description: "Історія про потойбічний світ",
+  },
+  blackclover: {
+    title: "Black Clover",
+    episodes: 170,
+    description: "Пригоди хлопчика без магії в світі магії",
+  },
+  drstone: {
+    title: "Dr.Stone",
+    episodes: 70,
+    description: "Наука в кам'яному віці?",
+  },
+  vanitasnokarte: {
+    title: "Vanitas no Karte",
+    episodes: 24,
+    description: "Історія про вампірів",
+  },
+  sololeveling: {
+    title: "Solo Leveling",
+    episodes: 25,
+    description: "Найслабший стає найсильнішим?",
+  },
+};
