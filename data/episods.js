2f51e963345d897466272ed27dfb309d93e74216
Dimoochik28 <148957099+Dimoochik28@users.noreply.github.com>
2025-05-20 23:30:58 +0300

    Create episods.js

---
+++ data/episods.js
@@ -0,0 +1,115 @@
+window.animeData = {
+  onepiece: {
+    title: "One Piece",
+    description: "Пригоди піратів у пошуках One Piece.",
+    cover: "Images/logo_one_piece",
+    seasons: [
+      {
+        seasonTitle: "",
+        episodes: 1127,
+      },
+    ],
+  },
+  naruto: {
+    title: "Naruto",
+    description: "Історія про ніндзя",
+    cover: "Images/logo_naruto.jpg",
+    seasons: [
+      {
+        seasonTitle: "",
+        episodes: 220,
+      },
+      {
+        seasonTitle: "",
+        episodes: 500,
+      },
+    ],
+  },
+  boruto: {
+    title: "Boruto",
+    description: "Продовження історії про ніндзя",
+    cover: "Images/logo_boruto.jpg",
+    seasons: [
+      {
+        seasonTitle: "",
+        episodes: 239,
+      },
+    ],
+  },
+  bleach: {
+    title: "Bleach",
+    description: "Історія про потойбічний світ",
+    cover: "Images/logo_bleach.webp",
+    seasons: [
+      {
+        seasonTitle: "",
+        episodes: 366,
+      },
+    ],
+  },
+  blackclover: {
+    title: "Black Clover",
+    description: "Пригоди хлопчика без магії в світі магії",
+    cover: "Images/logo_black_clover.jpg",
+    seasons: [
+      {
+        seasonTitle: "",
+        episodes: 170,
+      },
+    ],
+  },
+  drstone: {
+    title: "Dr.Stone",
+    description: "Наука в кам'яному віці?",
+    cover: "Images/logo_dr.Stone.jpg",
+    seasons: [
+      {
+        seasonTitle: "",
+        episodes: 24,
+      },
+
+      {
+        seasonTitle: "",
+        episodes: 12,
+      },
+      {
+        seasonTitle: "",
+        episodes: 22,
+      },
+      {
+        seasonTitle: "",
+        episodes: 12,
+      },
+    ],
+  },
+  vanitasnokarte: {
+    title: "Vanitas no Karte",
+    description: "Історія про вампірів",
+    cover: "Images/logo_memuars_of_vanitase.png",
+    seasons: [
+      {
+        seasonTitle: "",
+        episodes: 12,
+      },
+      {
+        seasonTitle: "",
+        episodes: 12,
+      },
+    ],
+  },
+  sololeveling: {
+    title: "Solo Leveling",
+    description: "Найслабший стає найсильнішим?",
+    cover: "Images/logo_solo_leveling.jpg",
+    seasons: [
+      {
+        seasonTitle: "",
+        episodes: 12,
+      },
+      {
+        seasonTitle: "",
+        episodes: 13,
+      },
+    ],
+  },
+};
